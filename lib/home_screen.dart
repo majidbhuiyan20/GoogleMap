0aca58807bd98da5ed54000c2e81349afb23eea9
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-12-07 01:18:29 +0600

    update google map

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,11 +9,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController _mapController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(23.766858859044362, 90.41741492396856))),
-
+      body: GoogleMap(
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        initialCameraPosition: CameraPosition(
+          zoom: 16,
+          target: LatLng(23.766858859044362, 90.41741492396856),
+        ),
+        onMapCreated: (GoogleMapController controller){
+          _mapController = controller;
+        },
+      ),
     );
   }
 }
