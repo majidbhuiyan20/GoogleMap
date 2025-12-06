6e182392d362862f23edd34900efdc87e220029a
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-12-07 00:52:08 +0600

    init google map on the map

---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: LatLng(23.766858859044362, 90.41741492396856))),
+
+    );
+  }
+}
